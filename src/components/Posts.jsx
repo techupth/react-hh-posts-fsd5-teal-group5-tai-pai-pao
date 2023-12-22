08e88b378463841fc7922489a2b9932f71d81da5
Pao <chaiwatt77@gmail.com>
2023-12-22 16:34:21 +0700

    ex2

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,30 +1,56 @@
+import listDetails from "./list";
+import { useState } from 'react';
+
 function Posts() {
+  const [counter,setCounter] = useState(listDetails);
+
+  const addLike = (index)=>{
+    const newCounter = [...counter];//ถ้าใช้ const newCounter = counter จะขึ้นแค่ใน console.log แต่หน้าจอไม่เพิ่ม
+    newCounter[index].likes += 1
+    console.log(newCounter[index].likes)
+    setCounter(newCounter);
+}
+
+const disLike = (index)=>{
+  const newCounter = [...counter];
+  if(newCounter[index].likes > 0){
+    newCounter[index].likes -= 1
+  }
+  console.log(newCounter[index].likes)
+  setCounter(newCounter);
+}
+
   return (
-    <div class="app-wrapper">
+    counter.map((listObj,index)=>{
+      return (
+        <div class="app-wrapper" key={index}>
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
         <div class="post-item">
           <div class="post-header">
-            <h2>Post Title #1</h2>
+            <h2>{listObj.title} #{listObj.id}</h2>
             <div class="post-social-media-stats">
               <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+              <span class="post-likes">{listObj.likes}</span>
             </div>
           </div>
           <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
+            {listObj.content}
           </p>
           <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
+            <button class="like-button" onClick = {()=>{
+              addLike(index);
+            }}>Like</button>
+
+            <button class="dislike-button" onClick = {()=>{
+              disLike(index);
+            }}>Dislike</button>
           </div>
         </div>
       </div>
     </div>
+      )
+    })
   );
 }
 
